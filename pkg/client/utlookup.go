759541e747c62b03609555bec97966598e6e8a84
Timothy Moore <mtimothy984@gmail.com>
2021-06-09 12:37:30 -0700

    Make UnitTypeLookup more versatile

--- pkg/client/utlookup.go
+++ pkg/client/utlookup.go
@@ -6,27 +6,38 @@ type UnitTypeLookup map[string]map[string]struct{}
 
 // Add the given smart object to this index
 func (l UnitTypeLookup) Add(obj *SmartObject) {
-	set, found := l[obj.UnitType]
+	l.AddUID(obj.UnitType, obj.GameObject.UID)
+}
+
+// AddUID adds the smart object with the given uid to this index
+func (l UnitTypeLookup) AddUID(unitType string, uid string) {
+	set, found := l[unitType]
 	if !found {
 		set = make(map[string]struct{})
-		l[obj.UnitType] = set
+		l[unitType] = set
 	}
-	set[obj.GameObject.UID] = struct{}{}
+	set[uid] = struct{}{}
 }
 
 // Remove the given smart object from this index
 func (l UnitTypeLookup) Remove(obj *SmartObject) {
-	set, found := l[obj.UnitType]
+	l.RemoveUID(obj.UnitType, obj.GameObject.UID)
+}
+
+// RemoveUID removes the smart object with the given type and uid from
+// this index
+func (l UnitTypeLookup) RemoveUID(unitType string, uid string) {
+	set, found := l[unitType]
 	if !found {
 		panic("smart object not in lookup")
 	}
-	if _, ok := set[obj.GameObject.UID]; !ok {
+	if _, ok := set[uid]; !ok {
 		panic("smart object not in lookup")
 	}
 
-	delete(set, obj.GameObject.UID)
+	delete(set, uid)
 	if len(set) == 0 {
-		delete(l, obj.UnitType)
+		delete(l, unitType)
 	}
 }
 
